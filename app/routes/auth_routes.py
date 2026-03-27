994362770d0f4e3cd77debd157417e5abf412c80
Everton Paulino <evertonpaulino25@hotmail.com>
2026-03-26 23:58:23 -0300

    update auth routes

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,22 +1,28 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
 from app.services.auth_service import register, login
+from flask import Blueprint, request, jsonify
+
+from flask import Blueprint, request, jsonify
 
 auth_bp = Blueprint('auth', __name__)
 
-@auth_bp.route('/register', methods=['POST'])
-def register_user():
-    try:
-        user = register(request.json)
-        return jsonify({'msg': 'User created'}), 201
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+# 🔥 variável global (SÓ PRA TESTE)
+user_logged = None
+
 
 @auth_bp.route('/login', methods=['POST'])
-def login_user():
-    try:
-        user = login(request.json)
-        token = create_access_token(identity=user.id)
-        return jsonify({'access_token': token})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 401
+def login_post():
+    global user_logged
+
+    data = request.json
+    user_logged = data.get("username")
+
+    return jsonify({"msg": "login ok"})
+
+
+@auth_bp.route('/login', methods=['GET'])
+def login_get():
+    return jsonify({
+        "user": user_logged
+    })
