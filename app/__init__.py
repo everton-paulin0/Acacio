63dbd960516a25b91361d8504c96fe4120714403
Everton Paulino <evertonpaulino25@hotmail.com>
2026-03-26 23:32:08 -0300

    update fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flasgger import Swagger
 from .extensions import db, migrate, jwt
 from .config import Config
 from .routes import register_routes
+from flasgger import Swagger
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    swagger = Swagger(app)  # 👈 muda pra essa forma
+
+    print(app.url_map)  # 👈 DEBUG (MUITO IMPORTANTE)
 
     db.init_app(app)
     migrate.init_app(app, db)
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,6 @@
 from .auth_routes import auth_bp
+from .health import health_bp
 
 def register_routes(app):
     app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(health_bp)
